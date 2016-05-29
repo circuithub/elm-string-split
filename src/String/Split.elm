d63bf63114301b02ba96aa3368d945e1a1313a99
Eduard Kyvenko <edduard@dinero.dk>
2016-05-29 17:47:49 +0200

    Applied the latest code standard

--- src/String/Split.elm
+++ src/String/Split.elm
@@ -1,4 +1,5 @@
 module String.Split exposing (chunksOfLeft, chunksOfRight)
+
 {-| Split strings into chunks
 
 # Splitters
@@ -9,16 +10,22 @@ module String.Split exposing (chunksOfLeft, chunksOfRight)
 import String exposing (..)
 import List
 
+
 {-| Split string into smaller strings of length `k`, starting from the left.
 
     chunksOfLeft 3 "abcdefgh" == ["abc", "def", "gh"]
 -}
 chunksOfLeft : Int -> String -> List String
 chunksOfLeft k s =
-  let len = length s
-  in  if len > k
-      then left k s :: chunksOfLeft k (dropLeft k s)
-      else [s]
+    let
+        len =
+            length s
+    in
+        if len > k then
+            left k s :: chunksOfLeft k (dropLeft k s)
+        else
+            [ s ]
+
 
 {-| Split string into smaller strings of length `k`, starting from the right.
 
@@ -26,15 +33,22 @@ chunksOfLeft k s =
 -}
 chunksOfRight : Int -> String -> List String
 chunksOfRight k s =
-  let len = length s
-      k2 = 2 * k
-      chunksOfR s' =
-        if length s' > k2
-        then right k s' :: chunksOfR (dropRight k s')
-        else right k s' :: [dropRight k s']
-  in  if len > k2 then
-          List.reverse (chunksOfR s)
-      else if len > k then
-          dropRight k s :: [right k s]
-      else
-          [s]
+    let
+        len =
+            length s
+
+        k2 =
+            2 * k
+
+        chunksOfR s' =
+            if length s' > k2 then
+                right k s' :: chunksOfR (dropRight k s')
+            else
+                right k s' :: [ dropRight k s' ]
+    in
+        if len > k2 then
+            List.reverse (chunksOfR s)
+        else if len > k then
+            dropRight k s :: [ right k s ]
+        else
+            [ s ]
